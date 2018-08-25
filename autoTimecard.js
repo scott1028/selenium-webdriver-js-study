27a56ff53cb52b82aa015e07961608f704b8ace7
scott <scott.lan@taisys.com>
2018-08-25 12:06:36 +0800

    add complex action test; high speed execute automation script

--- autoTimecard.js
+++ autoTimecard.js
@@ -4,24 +4,42 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
   // http://chromedriver.storage.googleapis.com/index.html?path=2.41/
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    // await driver.get('http://www.google.com/ncr');
-    await driver.get('http://www.wibibi.com/info.php?tid=194');
-    // var elm = await driver.findElement(By.tagName('body'));
-    // await elm.getText().then(text => console.log(`Text is ${text}`));
-    // await elm.getAttribute('innerHTML').then(text => console.log(`Text is ${text}`));
-    
-    // await driver.findElement(By.tagName('option')).then(r => console.log(r));
-    // driver.findElement(By.id('CodeBox1'))
-    await driver.findElement(By.id('CodeBox1'))
-                .findElement(By.tagName('select')).click();
-    await driver.findElement(By.id('CodeBox1'))
-                .findElement(By.tagName('select'))
-                .findElement(By.tagName('option'));
-    // select third option
-    await driver.findElement(By.xpath('//*[@id="CodeBox1"]/select/option[3]'))
-                .click();
-    // debugger;
+    // [Start]
+    await driver.get('http://127.0.0.1:8080/timecard.html');
+
+    // [Detect If Document Ready]
+    // var val = await driver.executeScript(cb => {  // no cb support premise .executeScript
+    var val = await driver.executeAsyncScript(cb => {
+      $(document).ready(() => {
+        console.log(`[Document] ${document.readyState}`);
+        cb(100);
+      });
+    }).then(data => `[Document][Ready]${data}`); console.log(val);
+
+    // Click some DOM
+    await driver.findElement(By.id('type')).click();
+    await driver.findElement(By.xpath('//*[@id="type"]/option[3]')).click();
+    await driver.findElement(By.id('type')).click();
+    await driver.findElement(By.xpath('//*[@id="modal"]')).click();
+
+    await driver.getAllWindowHandles().then(async h => {
+      // change to new window
+      await driver.switchTo().window(h.pop());
+      await driver.findElement(By.tagName('body')).getAttribute('innerHTML').then(text => console.log(`Text is ${text}`));
+      await driver.findElement(By.xpath('//*[@id="desc"]')).sendKeys(` - ${new Date().getTime()}`);
+      await driver.findElement(By.xpath('//*[@id="desc"]')).clear();
+      await driver.findElement(By.xpath('//*[@id="desc"]')).sendKeys(`Get Now Time => ${new Date().getTime()}`);
+      await driver.findElement(By.xpath('//*[@id="closeBtn"]')).click();
+
+      // back to original window
+      await driver.switchTo().window(h.pop());
+      await driver.findElement(By.xpath('//*[@id="myForm"]/table/tbody/tr[1]/td/input')).sendKeys('myUsername');
+      await driver.findElement(By.xpath('//*[@id="myForm"]/table/tbody/tr[2]/td/input')).sendKeys('myPassword');
+      await driver.findElement(By.xpath('//*[@id="myForm"]/table/tbody/tr[4]/th[2]/input')).click();
+      await driver.findElement(By.xpath('//*[@id="myForm"]/table/tbody/tr[6]/td/button')).click();
+    });
   } finally {
-    await driver.quit();
+    // await driver.quit();  // close all browser
+    console.log(`[DEBUG] All Test Done!`);
   }
 })();
